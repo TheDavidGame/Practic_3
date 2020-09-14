5e5df44be6ff1570335ab73e60c235e592781b07
TheDavidGame <vlad.billI@bk.ru>
2020-09-14 22:30:59 +0300

    ex_1
    =)

---
+++ src/com/Pract_3/Circle.java
@@ -0,0 +1,41 @@
+package com.Pract_3;
+
+public class Circle {
+    private double S = 0;
+    private double V = 0;
+    private double R = 0;
+
+    public void setR(double r) {
+        R = r;
+    }
+
+    public void setS(double s) {
+        S = s;
+    }
+
+    public void setV(double v) {
+        V = v;
+    }
+
+    public double getS() {
+        return S;
+    }
+
+    public double getV() {
+        return V;
+    }
+
+    public double getR() {
+        return R;
+    }
+
+    public static void Radius(double R ) {
+
+        System.out.println("R: "  + (Math.PI + Math.pow(R,2)));
+    }
+ 
+    public static void V(float PI, float R) {
+        System.out.println("V: " + ((4 / 3) * Math.PI * Math.pow(R,3)));
+    }
+
+}
--- src/com/Pract_3/Main.java
+++ src/com/Pract_3/Main.java
@@ -1,8 +1,11 @@
 package com.Pract_3;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+    Circle c1 = new Circle();
+    c1.setR(15);
+        System.out.println(c1.getR());
+    c1.Radius(15);
     }
 }
